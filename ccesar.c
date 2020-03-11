079c25b2e9c854a8978375503a950bd1d653754b
igorbavand <igor.bvn@gmail.com>
2020-03-11 20:32:25 -0300

    resultado final

--- ccesar.c
+++ ccesar.c
@@ -1,9 +1,8 @@
 //José Igor de Sousa - 404735
 #include <stdio.h>
-#include<stdlib.h>
 
-int main()
-{
+
+int main(void){
     void criptografar(){
         char alfabeto[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
         char palavra[200];
@@ -18,55 +17,51 @@ int main()
         puts("digite o que deseja criptografar: ");
         fgets(palavra, 200, stdin);
 
-        while (palavra[tamanho_palavra] != '\0')
-        {
+        while (palavra[tamanho_palavra] != '\0'){//tamanho da palavra
             tamanho_palavra++;
         }
         tamanho_palavra = tamanho_palavra - 1;
         char palavra_criptografada[200];
-        int i = 0, l = 0, cont = 0;
-        do{ //para percorrer a string
+        int i = 0, l = 0;
+        do{ 
             l = 0;
-            do{ //percorrer o alfabeto
-                if (palavra[i] == ' ')
-                {
+            do{ 
+                if (palavra[i] == ' '){//verificar se tem espaços
                     palavra_criptografada[i] = ' ';
-                    cont++;
                 }
-                else
-                {
+                else if (palavra[i] != 'a' && palavra[i] != 'b' && palavra[i] != 'c' && palavra[i] != 'd' && palavra[i] != 'e' && palavra[i] != 'f' && palavra[i] != 'g' && palavra[i] != 'h' && palavra[i] != 'i' && palavra[i] != 'j' && palavra[i] != 'k' && palavra[i] != 'l' && palavra[i] != 'm' && palavra[i] != 'n' &&
+                         palavra[i] != 'o' && palavra[i] != 'p' && palavra[i] != 'q' && palavra[i] != 'r' && palavra[i] != 's' && palavra[i] != 't' && palavra[i] != 'u' &&
+                         palavra[i] != 'v' && palavra[i] != 'w' && palavra[i] != 'x' && palavra[i] != 'y' && palavra[i] != 'z'){
+                    palavra_criptografada[i] = palavra[i];// verifica se não é uma letra do alfabeto, e apenas adiciona, e pula para proxima
+                }
+                else{
 
-                    if (palavra[i] == alfabeto[l])
-                    { //procurar a posicao da letra da palavra no alfabeto
+                    if (palavra[i] == alfabeto[l]){// verifica qual posição está a letra 
                         int k = 0;
                         posi = l + 1;
-                        do{ //quantas posições ele vai andar para frente no alfabeto
+                        do{ 
                             posi++;
-                            if (posi > 26)
-                            { //quando passar de 'z' zera a posição
+                            if (posi > 26){//se passar de 'z' volta para a posição inicial
                                 posi = 0;
                             }
                             k++;
                         }while (k < chave);
-                        if (posi > l)
-                        { //sempre que passa de 'z' para 'a' ele começa de 1, então decrementa para 0
+                        if (posi > l){//sempre que ele passa de 'z' para 'a' pega uma posição a mais, por isso posi = -1
                             posi = posi - 1;
                         }
-                        palavra_criptografada[i] = alfabeto[posi]; //pegando a letra da posição da chave
+                        palavra_criptografada[i] = alfabeto[posi];// quando vetor recebe a posição da criptografia
                     }
                 }
                 l++;
             } while (l < 26);
                 i++;
-        } while (i < tamanho_palavra);
+        } while (i < tamanho_palavra); 
 
-        palavra_criptografada[tamanho_palavra] = '\0'; // para palvras sem espaços
-        
+        palavra_criptografada[tamanho_palavra] = '\0'; 
 
+        puts("\n\n");
         printf("A criptografia e: %s\n\n", palavra_criptografada);
-        // printf("%d", tamanho_criptografia);
-
-        //a ligeira raposa marrom saltou sobre o cachorro cansado
+        
         
     }
 
@@ -83,63 +78,51 @@ int main()
         puts("digite o que deseja descriptografar: ");
         fgets(palavra, 200, stdin);
 
-        while (palavra[tamanho_palavra] != '\0')
-        {
+        while (palavra[tamanho_palavra] != '\0'){
             tamanho_palavra++;
         }
         tamanho_palavra = tamanho_palavra - 1;
         char palavra_descriptografada[200];
 
-        int i = 0, l = 0, cont = 0;
-        do
-        { //para percorrer a string
+        int i = 0, l = 0;
+        do{ 
             l = 0;
-            do
-            { //percorrer o alfabeto
-                if (palavra[i] == ' ')
-                {
+            do{ 
+                if (palavra[i] == ' '){
                     palavra_descriptografada[i] = ' ';
-                    cont++;
-                }else if(){
-
-                }else
-                {
-
+                }
+                else if (palavra[i] != 'a' && palavra[i] != 'b' && palavra[i] != 'c' && palavra[i] != 'd' && palavra[i] != 'e' && palavra[i] != 'f' && palavra[i] != 'g' && palavra[i] != 'h' && palavra[i] != 'i' && palavra[i] != 'j' && palavra[i] != 'k' && palavra[i] != 'l' && palavra[i] != 'm' && palavra[i] != 'n' &&
+                         palavra[i] != 'o' && palavra[i] != 'p' && palavra[i] != 'q' && palavra[i] != 'r' && palavra[i] != 's' && palavra[i] != 't' && palavra[i] != 'u' &&
+                         palavra[i] != 'v' && palavra[i] != 'w' && palavra[i] != 'x' && palavra[i] != 'y' && palavra[i] != 'z'){
+                    palavra_descriptografada[i]= palavra[i];
+                }else{
                     if (palavra[i] == alfabeto[l])
-                    { //procurar a posicao da letra da palavra no alfabeto
+                    { 
                         int k = 0;
                         posi = l ;
-                        do
-                        { //quantas posições ele vai andar para frente no alfabeto
+                        do{ 
                             posi--;
-                            if (posi < 0)
-                            { //quando passar de 'z' zera a posição
+                            if (posi < 0){ 
                                 posi = 25;
                             }
                             k++;
                         } while (k < chave);
-                        /*if (posi > l)
-                        { //sempre que passa de 'z' para 'a' ele começa de 1, então decrementa para 0
-                            posi = posi - 1;
-                        }*/
-                        palavra_descriptografada[i] = alfabeto[posi]; //pegando a letra da posição da chave
+                        palavra_descriptografada[i] = alfabeto[posi];
                     }
                 }
                 l++;
             } while (l < 26);
             i++;
         } while (i < tamanho_palavra);
-        palavra_descriptografada[tamanho_palavra] = '\0'; // para palvras sem espaços
-
-        printf("A criptografia e: %s\n\n", palavra_descriptografada);
+        palavra_descriptografada[tamanho_palavra] = '\0'; 
+        puts("\n\n");
+        printf("A descriptografia e: %s\n\n", palavra_descriptografada);
     }
     int opc=0;
-    
 
-    
+   
     do{
-        
-        puts("*********************Cifra de Cesar**********************");
+        puts("********************-Cifra de Cesar-*********************");
         puts("|\t\t\t\t\t\t\t|");
         puts("|\t1 - Criptografar\t\t\t\t|");
         puts("|\t2 - Descriptografar\t\t\t\t|");
@@ -148,8 +131,8 @@ int main()
         puts("*********************************************************");
         puts("Digite a opcao: ");
         scanf("%d", &opc);
-
-    if(opc == 1){
+        
+    if (opc == 1){
         criptografar();
     }else if(opc == 2){
         decript();
@@ -161,6 +144,4 @@ int main()
 
     }while(opc != 3);
     
-
-        return 0;
 }
